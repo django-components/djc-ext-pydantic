f8c0508db0aec2bdfd8f6b436b64f1ae6b1eb99d
Juro Oravec <juraj.oravec.josefson@gmail.com>
2025-04-05 09:19:58 +0200

    refactor: fix linters

--- src/djc_pydantic/__init__.py
+++ src/djc_pydantic/__init__.py
@@ -1,6 +1,5 @@
-from djc_pydantic.monkeypatch import monkeypatch_pydantic_core_schema
 from djc_pydantic.extension import PydanticExtension
-
+from djc_pydantic.monkeypatch import monkeypatch_pydantic_core_schema
 
 monkeypatch_pydantic_core_schema()
 
